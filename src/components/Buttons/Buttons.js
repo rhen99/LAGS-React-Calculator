9df521b4c6b0119dd946a180a3fb6319e57a8a4a
Arhen Santiago <lincoldarhen9900@gmail.com>
2021-04-09 16:17:22 +0800

    styling finished

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -1,10 +1,69 @@
-import './Buttons.css'
-function Buttons() {
+
+import './Buttons.css';
+function Buttons({isOn, power}) {
     return (
         <div className="Buttons">
-            
+            <div className="Button">
+                <button className="Number">7</button>
+            </div>
+            <div className="Button">
+                <button className="Number">8</button>
+            </div>
+            <div className="Button">
+                <button className="Number">9</button>
+            </div>
+            <div className="Button">
+                <button className="Function">+/-</button>
+            </div>
+            <div className="Button">
+                <button className="Function">C</button>
+            </div>
+            <div className="Button">
+                <button className="Number">4</button>
+            </div>
+            <div className="Button">
+                <button className="Number">5</button>
+            </div>
+            <div className="Button">
+                <button className="Number">6</button>
+            </div>
+            <div className="Button">
+                <button className="Operator">&times;</button>
+                
+            </div>
+            <div className="Button">
+                <button className="Operator">&divide;</button>
+            </div>
+            <div className="Button">
+                <button className="Number">1</button>
+            </div>
+            <div className="Button">
+                <button className="Number">2</button>
+            </div>
+            <div className="Button">
+                <button className="Number">3</button>
+            </div>
+            <div className="Button">
+                <button className="Operator">-</button>
+            </div>
+            <div className="Button">
+                <button className="Equals">=</button>
+            </div>
+            <div className="Button">
+                <button onClick={power} className={isOn ? "Power off": "Power on"}>{isOn ? "OFF": "ON"}</button>
+            </div>
+            <div className="Button">
+                <button className="Number">0</button>
+            </div>
+            <div className="Button">
+                <button className="Number">.</button>
+            </div>
+            <div className="Button">
+                <button className="Operator">+</button>
+            </div>
+            <div className="Button"></div>
         </div>
     )
 }
 
-export default Buttons
+export default Buttons;
--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,12 +1,18 @@
+import { useState } from 'react'
 import Brand from '../Brand/Brand';
 import Screen from '../Screen/Screen';
+import Buttons from '../Buttons/Buttons';
 
 import './Calculator.css'
 function Calculator() {
+    const [isOn, setIsOn] = useState(false);
+
+    const power = () => setIsOn(prev => !prev);
     return (
         <div className="Calculator">
             <Brand/>
             <Screen/>
+            <Buttons isOn={isOn} power={power}/>
         </div>
     )
 }
